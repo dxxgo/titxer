8c6d443fe03ba65e5319c022b2aa85d66c18e28c
Diêgo Ferreira <diegofrr.19@gmail.com>
2022-06-09 17:06:29 -0300

    att

--- src/Pages/Login/index.js
+++ src/Pages/Login/index.js
@@ -7,7 +7,7 @@ import { Link } from 'react-router-dom';
 export default function Login() {
     return (
         <div className="login-container">
-            <Link className='voltarLink' to='/titxer/'>
+            <Link className='voltarLink' to='/titxer/'> 
                 <img src={voltarIcon} />
                 voltar</Link>
             <div className='login-content'>
